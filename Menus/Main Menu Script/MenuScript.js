7b30b6900580bc84e4e04fe194ece57bf340f7b4
Brendan Quail <bquail10@caledonian.ac.uk>
2013-02-19 10:39:49 +0000

    The Main Menu and art assets
    This has the 4 buttons included on main menu screen.

--- Menus/Main Menu Script/MenuScript.js
+++ Menus/Main Menu Script/MenuScript.js
@@ -1,12 +1,12 @@
 #pragma strict
-var GuiTexture : Texture2D;
 var gamestate : int = 1;
-var howtoTexture : Texture;
+var moreinfoTexture : Texture;
+var highscoreTexture : Texture;
 var menuTexture : Texture;
-//public var menuPlane : GameObject;
 var startSkin: GUISkin;
-var howtoSkin: GUISkin;
+var moreinfoSkin: GUISkin;
 var exitSkin: GUISkin;
+var highscoreSkin: GUISkin;
  
 function Start () {
 
@@ -23,19 +23,25 @@ function OnGUI() {
 		GUI.DrawTexture(Rect(0,0,Screen.width,Screen.height),menuTexture);
 		
     	GUI.skin = startSkin;
-    	if (GUI.Button(Rect(Screen.width/10 ,Screen.height/2 ,Screen.width/5,Screen.height/10)," "))
+    	if (GUI.Button(Rect(Screen.width/10 ,Screen.height/4.5 ,Screen.width/5,Screen.height/10)," "))
     	{
         	Application.LoadLevel("lighting");
     	}
     	
-    	GUI.skin = howtoSkin;
-    	if (GUI.Button(Rect(Screen.width/10 ,Screen.height/2 + 75 ,Screen.width/5,Screen.height/10)," "))
+    	GUI.skin = moreinfoSkin;
+    	if (GUI.Button(Rect(Screen.width/10 ,Screen.height/3.5+50 ,Screen.width/5,Screen.height/10)," "))
     	{
         	gamestate=2;
     	}   
     	
+    	GUI.skin = highscoreSkin;
+    	if (GUI.Button(Rect(Screen.width/10 ,Screen.height/2.5+50 ,Screen.width/5,Screen.height/10)," "))
+    	{
+    		gamestate=3;
+    	}
+    	
     	GUI.skin = exitSkin;
-    	if (GUI.Button(Rect(Screen.width/10 ,Screen.height/2 + 150 ,Screen.width/5,Screen.height/10)," "))
+    	if (GUI.Button(Rect(Screen.width/10 ,Screen.height/1.5-30,Screen.width/5,Screen.height/10)," "))
     	{
        		Application.Quit();
     	}
@@ -43,25 +49,22 @@ function OnGUI() {
     
     if(gamestate == 2)
     {   
-    	GUI.DrawTexture(Rect(0,0,Screen.width,Screen.height),howtoTexture);
+    	GUI.DrawTexture(Rect(0,0,Screen.width,Screen.height),moreinfoTexture);
     	GUI.skin = exitSkin;
     	if (GUI.Button(Rect(Screen.width/10 ,Screen.height/1.3 ,Screen.width/5,Screen.height/10)," "))
     	{
        		gamestate=1;
     	}
     }
-    
+   
+     if(gamestate == 3)
+    {   
+    	GUI.DrawTexture(Rect(0,0,Screen.width,Screen.height),highscoreTexture);
+    	GUI.skin = exitSkin;
+    	if (GUI.Button(Rect(Screen.width/10 ,Screen.height/1.3 ,Screen.width/5,Screen.height/10)," "))
+    	{
+       		gamestate=1;
+    	}
+    } 
    
  }
- 
-/*
-function ResizeGUI(_rect : Rect) : Rect
-{
-   	var filScreenWidth = _rect.width / 800;
-   	var rectWidth = filScreenWidth * Screen.width;
-   	var filScreenHeight = _rect.height / 600;
-   	var rectHeight = filScreenHeight * Screen.height;
-   	var rectX = (_rect.x / 800) * Screen.width;
-   	var rectY = (_rect.x / 600) * Screen.height;
-}
-*/
